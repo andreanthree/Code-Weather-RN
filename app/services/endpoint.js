acf504c94ff9f094099ef308a91f9cb227b8e8ab
Andrean Three Saputra <andreanthree1@gmail.com>
2022-12-22 01:28:07 +0700

    feat: setup networking

---
+++ app/services/endpoint.js
@@ -0,0 +1,7 @@
+import { post, get, remove, put, patch } from "./networking";
+
+export const endpoint = {
+  
+};
+
+export default { endpoint };
---
+++ app/services/networking.js
@@ -0,0 +1,79 @@
+
+import axios from 'axios';
+import _ from 'lodash';
+
+export const config = {
+  baseUrl: 'https://api.openweathermap.org',
+};
+const Authorization = {};
+export const STATUS_CODE = {
+  OK: 200,
+  NOT_FOUND: 404,
+  NO_CONTENT: 204,
+};
+
+const fetchData = async (url, params, customHeaders) => {
+  let headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    ...customHeaders,
+  };
+  const response = await axios(url, ...params, headers);
+  const json = await response.json();
+
+
+  return json;
+};
+
+const get = async (endpoint, params = {}, headers = {}) => {
+  let queryString = Object.keys(params)
+    .map(key => `${key}=${params[key]}`)
+    .join('&');
+  if (queryString.length > 0) {
+    queryString = `?${queryString}`;
+  }
+  const url = `${config.baseUrl}${endpoint}${queryString}`;
+  const fetchParams = {
+    method: 'GET',
+  };
+
+  return fetchData(url, fetchParams, headers);
+};
+
+const post = async (endpoint, params = {}, headers = {}) => {
+  const url = `${config.baseUrl}${endpoint}`;
+  const fetchParams = {
+    method: 'POST',
+    body: params,
+  };
+  return fetchData(url, fetchParams, headers);
+};
+
+const patch = async (endpoint, params = {}, headers = {}) => {
+  const url = `${config.baseUrl}${endpoint}`;
+  const fetchParams = {
+    method: 'PATCH',
+    body: params,
+  };
+  return fetchData(url, fetchParams, headers);
+};
+
+const put = async (endpoint, params = {}, headers = {}) => {
+  const url = `${config.baseUrl}${endpoint}`;
+  const fetchParams = {
+    method: 'PUT',
+    body: params,
+  };
+  return fetchData(url, fetchParams, headers);
+};
+
+const remove = async (endpoint, params = {}, headers = {}) => {
+  const url = `${config.baseUrl}${endpoint}`;
+  const fetchParams = {
+    method: 'DELETE',
+    body: params,
+  };
+  return fetchData(url, fetchParams, headers);
+};
+
+export {get, post, put, patch, remove};
